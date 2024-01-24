ae2e4bbf499dbccb08c920ca27acfea4e32b4889
seeewo4kin <117713764+seeewo4kin@users.noreply.github.com>
2024-01-24 15:25:03 +0300

    Merge pull request #2 from seeewo4kin/revert-1-1.1.4
     added hibernate util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -20,7 +20,7 @@ public class Util {
     private static Connection connection = null;
     private static Util instance = null;
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/mydbtest";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Sewo4kin123";
 
@@ -54,7 +54,7 @@ public class Util {
 
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://127.0.0.1:3306/mydbtest");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest");
                 settings.put(Environment.USER, "root");
                 settings.put(Environment.PASS, "Sewo4kin123");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
